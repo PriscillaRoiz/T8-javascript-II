3f55496abd1cd84bb8dff6d28f1fc171ed6d5e4c
PriscillaRoiz <priscillagarcia02@hotmail.com>
2019-09-25 16:31:13 -0300

    add exercicio extra

---
+++ Exercício-extra/js/script.js
@@ -0,0 +1,35 @@
+const formulario = document.getElementById('formulario-de-comentarios');
+
+formulario.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+    let inputUsuario = document.querySelector(".input-de-texto");
+    let mensagem = inputUsuario.value;
+
+    // console.log (inputUsuario);
+    // console.log (inputUsuario.value);
+    // ou inputUsuario.value = "";
+
+    if (mensagem === "") {
+
+        console.error("You made a mistake");
+        alert.error("You made a mistake");
+
+        //ou 
+
+    erro.textContent = "You made a mistake"
+
+    }
+
+    else {
+
+        let comentarios = document.createElement("p");
+
+        comentarios.textContent = mensagem;
+        todosOsComentarios.appendChild(comentarios);
+        comentarios.classList.add("comentarios");
+
+        formulario.reset();
+    }
+});
+
+
